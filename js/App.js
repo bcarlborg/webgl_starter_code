3021f57a41e8689ddd5f3bf71fe328d78d35c043
bcarlborg <bcarlborg@gmail.com>
2019-09-23 19:19:29 +0200

    prettier

--- js/App.js
+++ js/App.js
@@ -1,16 +1,17 @@
-"use strict";
+
 /* exported App */
-class App{
+class App {
   constructor(canvas, overlay) {
     this.canvas = canvas;
     this.overlay = overlay;
 
-    this.gl = canvas.getContext("webgl2"); 
-  
+    this.gl = canvas.getContext('webgl2');
+
     if (this.gl === null) {
-      throw new Error("Browser does not support WebGL2");
+      throw new Error('Browser does not support WebGL2');
     }
-   
+
+    // eslint-disable-next-line no-undef
     this.scene = new Scene(this.gl);
 
     this.resize();
@@ -23,49 +24,50 @@ class App{
     this.scene.resize(this.gl, this.canvas);
   }
 
+  /* eslint-disable no-unused-vars */
   registerEventHandlers() {
     document.onkeydown = (event) => {
-      //jshint unused:false
+      // jshint unused:false
     };
     document.onkeyup = (event) => {
-      //jshint unused:false
+      // jshint unused:false
     };
     this.canvas.onmousedown = (event) => {
-      //jshint unused:false
+      // jshint unused:false
     };
     this.canvas.onmousemove = (event) => {
-      //jshint unused:false
+      // jshint unused:false
       event.stopPropagation();
     };
     this.canvas.onmouseout = (event) => {
-      //jshint unused:false
+      // jshint unused:false
     };
     this.canvas.onmouseup = (event) => {
-      //jshint unused:false
+      // jshint unused:false
     };
-    window.addEventListener('resize', () => this.resize() );
+    window.addEventListener('resize', () => this.resize());
 
     // asks the browser to execute a function every time a update an animation before a
     // repaint occurs on the screen. (This is the entry point for exectuing our code)
-    window.requestAnimationFrame( () => this.update() );
+    window.requestAnimationFrame(() => this.update());
   }
+  /* eslint-disable no-unused-vars */
 
   // animation frame update
   update() {
-
     this.scene.update(this.gl, this.keysPressed);
 
     // requestAnimationFrame must be called in this function
     // in order to access the next frame of the animation
-    window.requestAnimationFrame( () => this.update() );
+    window.requestAnimationFrame(() => this.update());
   }
 }
 
 // entry point from HTML -- the load listener is executed when all of the
 // page assests are downloaded (fully _loaded_)
 window.addEventListener('load', () => {
-  const canvas = document.getElementById("canvas");
-  const overlay = document.getElementById("overlay");
+  const canvas = document.getElementById('canvas');
+  const overlay = document.getElementById('overlay');
 
   const app = new App(canvas, overlay);
   app.registerEventHandlers();
