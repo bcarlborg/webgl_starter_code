651fd8347cbdcd8280eb8776be67cf32a0ed9bbf
bcarlborg <bcarlborg@gmail.com>
2019-09-26 00:00:34 +0200

    parametizing stripes

--- js/shaders/striped-fs.glsl
+++ js/shaders/striped-fs.glsl
@@ -5,14 +5,60 @@ Shader.source[document.currentScript.src.split('js/shaders/')[1]] = `#version 30
   in vec4 v_position;
 
   void main(void) {
-    float sinX = ceil(sin(v_position.x * 10.0) + 1.0);
-    float r;
+    float stripeDensity = 10.0;
+    float colorScheme = 0.0;
+
+    float sinX = ceil(sin(v_position.x * stripeDensity) + 1.0);
+
+    float myRedFirst;
+    float myGreenFirst;
+    float myBlueFirst;
+
+    float myRedSecond;
+    float myGreenSecond;
+    float myBlueSecond;
+
+    float redChannel;
+    float greenChannel;
+    float blueChannel;
+
+
+    if (colorScheme == 0.0) {
+      // nice Orange
+      myRedFirst = 0.894;
+      myGreenFirst = 0.341;
+      myBlueFirst = 0.18;
+
+      // nice blue 
+      myRedSecond = 0.49;
+      myGreenSecond = 0.74;
+      myBlueSecond = 0.76;
+    }
+
+    if (colorScheme == 1.0) {
+      // nice green
+      myRedFirst = 0.80;
+      myGreenFirst = 0.90;
+      myBlueFirst = 0.69;
+
+      // nice purple
+      myRedSecond = 0.55;
+      myGreenSecond = 0.35;
+      myBlueSecond = 0.59;
+    }
+
     if (sinX == 1.0) {
-      r = 0.5;
+      redChannel = myRedFirst;
+      greenChannel = myGreenFirst;
+      blueChannel = myBlueFirst;
     }
+
     if (sinX == 2.0) {
-      r = 0.1;
+      redChannel = myRedSecond;
+      greenChannel = myGreenSecond;
+      blueChannel = myBlueSecond;
     }
-    fragmentColor = vec4(r, 0.50, 0.50, 1);
+
+    fragmentColor = vec4(redChannel, greenChannel, blueChannel, 1);
   }
 `;
