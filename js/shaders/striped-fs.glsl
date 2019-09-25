6d8bd10dab2be224c2068564af8a211382ec5dae
bcarlborg <bcarlborg@gmail.com>
2019-09-25 23:44:16 +0200

    stripes in the fragment shader!

--- js/shaders/striped-fs.glsl
+++ js/shaders/striped-fs.glsl
@@ -5,7 +5,14 @@ Shader.source[document.currentScript.src.split('js/shaders/')[1]] = `#version 30
   in vec4 v_position;
 
   void main(void) {
-    float r = (0.5 * sin(v_position.x * 10.0)) + 0.5;
+    float sinX = ceil(sin(v_position.x * 10.0) + 1.0);
+    float r;
+    if (sinX == 1.0) {
+      r = 0.5;
+    }
+    if (sinX == 2.0) {
+      r = 0.1;
+    }
     fragmentColor = vec4(r, 0.50, 0.50, 1);
   }
 `;
